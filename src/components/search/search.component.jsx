35009c6db855efed5aa28adb89be2857af514605
Chandrakant <ckant912@gmail.com>
2022-04-19 16:31:19 +0530

    Rollodex app

---
+++ src/components/search/search.component.jsx
@@ -0,0 +1,16 @@
+import './search.styles.css'
+
+const Search = () => {
+
+    return (
+        <>
+            <input
+            className="search-box" 
+            type="text" 
+            placeholder="Search Monsters"
+            />
+        </>
+    )
+}
+
+export default Search
